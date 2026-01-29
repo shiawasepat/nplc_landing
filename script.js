6e2cddbe62f0822a2f6e8709a4f7b706172fcea2
Patrick <shiawasearuji@gmail.com>
2026-01-29 09:45:51 +0800

    date extend

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // GMT+8
-const targetDate = new Date('2026-02-18T00:00:00+08:00').getTime();
+const targetDate = new Date("2026-02-22T00:00:00").getTime();
 // const targetDate = new Date(Date.now() + 1 * 1 * 1 * 15 * 1000).getTime();
 
 function updateCountdown() {
-    const now = new Date().getTime();
-    const distance = targetDate - now;
+  const now = new Date().getTime();
+  const distance = targetDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('days').innerText = days;
-    document.getElementById('hours').innerText = hours;
-    document.getElementById('minutes').innerText = minutes;
-    document.getElementById('seconds').innerText = seconds;
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
 
-    if (distance <= 0) {
-        document.getElementById('days').innerText = 0;
-        document.getElementById('hours').innerText = 0;
-        document.getElementById('minutes').innerText = 0;
-        document.getElementById('seconds').innerText = 0;
-    }
+  if (distance <= 0) {
+    document.getElementById("days").innerText = 0;
+    document.getElementById("hours").innerText = 0;
+    document.getElementById("minutes").innerText = 0;
+    document.getElementById("seconds").innerText = 0;
+  }
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    countdownInterval = setInterval(updateCountdown, 1000);
+window.addEventListener("DOMContentLoaded", () => {
+  countdownInterval = setInterval(updateCountdown, 1000);
 });
